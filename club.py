54dbdf3ca3a229829d52f0422cea9d34499da793
OnOOF <19161295@itoaxaca.edu.mx>
2020-05-02 13:03:03 -0500

    subir

---
+++ club.py
@@ -0,0 +1,20 @@
+side=8
+for i in range(0,side+1):
+    print("*"*i)
+print()
+
+for i in range(0,side):
+    print("*"*side)
+print()
+
+for i in range(0,side):
+    sinasterisco=side-i
+    asterisco=i
+    print("*"*sinasterisco)
+
+for i in range(0,side+1):
+    print("*"*i)
+for i in range(0,side-1):
+    sinasterisco=side-i
+    asterisco=i
+    print("*"*sinasterisco)
